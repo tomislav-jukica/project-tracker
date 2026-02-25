ab6d37bceed2e100aeb2ed2f9a2dad2b532a5e48
Tomislav Jukica <tomislav.jukica07@gmail.com>
2026-02-25 21:05:09 +0100

    Add connection string to appsettings.

--- ProjectTracker.Api/Program.cs
+++ ProjectTracker.Api/Program.cs
@@ -1,9 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using ProjectTracker.Infrastructure.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AssetDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
